b0a9052d94138f4e7d2c1be7f7425ee570f9eec4
mobiustrip <yvan@metatech.ie>
2020-09-29 12:31:33 +0200

    added deadline datetime format to serializer

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "tamska.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "tamska.herokuapp.com", "taskma.net"]
 
 
 # Application definition
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -7,6 +7,8 @@ class TaskSerializer(serializers.ModelSerializer):
     groupId = serializers.IntegerField(source="group.id", read_only=True)
     groupTitle = serializers.CharField(source="group.title", read_only=True)
     dateCreated = serializers.DateTimeField(source="created_at")
+    deadline = serializers.DateTimeField(format="%Y-%m-%d %H:%M", required=False)
+
     description = serializers.CharField(required=False, allow_blank=True)
     state = serializers.IntegerField(required=False)
     archived = serializers.BooleanField(required=False)
